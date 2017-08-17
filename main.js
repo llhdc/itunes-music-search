e503f5ee78edde2310b0388fa7a2307165741173
Leonard <leonard.horne@gmail.com>
2017-08-17 12:09:45 -0400

    Fetch results, add event listener to fetched song titles.

--- main.js
+++ main.js
@@ -2,8 +2,61 @@
   Here is a rough idea for the steps you could take:
 */
 
-// 1. First select and store the elements you'll be working with
-// 2. Create your `submit` event for getting the user's search term
-// 3. Create your `fetch` request that is called after a submission
-// 4. Create a way to append the fetch results to your page
-// 5. Create a way to listen for a click that will play the song in the audio play
+// Declaring variables - url for data fetch; searchForm and searchInput to
+
+let searchForm = document.getElementById("search");
+let searchInput = searchForm.value;
+let main = document.querySelector("main");
+let button = document.querySelector("button");
+
+button.addEventListener("click", function(e) {
+  //prevent the normal submission of the form
+  e.preventDefault();
+  let url = `https://itunes.apple.com/search?term=${searchForm.value}&entity=musicTrack`;
+  console.log(searchForm);
+
+  fetch(url)
+    .then(response => {
+      if (response.status !== 200) {
+        console.log(response.status);
+        return;
+      }
+      response.json().then(data => {
+        // Declare variable for artist template container
+        let artistContainer = "";
+        // Iterate over data set and manipulate each data result
+        data.results.forEach(result => {
+          // Check if result.artworkUrl100 contains an img url and enter placeholder if not
+          if (result.artworkUrl100 === "") {
+            result.artworkUrl100 = "http://via.placeholder.com/165x80";
+          }
+          // Define template to use with data and enter into discrete container
+          let template = `<section class="result">
+                  <img class="art" src='${result.artworkUrl100}'>
+                  <span class="track">${result.trackName}</span>
+                  <p>${result.artistName}</p>
+                 </section>
+                `;
+          // For each data template, insert via concatenation to artistContainer
+          artistContainer += template;
+        });
+        // After each for each method executes insert template container into HTML.
+        main.innerHTML = artistContainer;
+
+        // Get an array of tracks, loop through the array adding an event listener to the track name so when clicked, will set the audio tag src attribute to the fetched data result 'previewUrl' attribute.
+
+        let tracks = document.getElementsByClassName("track");
+        let audio = document.getElementById("audio");
+
+        for (let i = 0; i < tracks.length; i++) {
+          tracks[i].addEventListener("click", function(e) {
+            audio.setAttribute("src", data.results[i].previewUrl);
+          });
+        }
+
+      });
+    })
+    .catch(function(err) {
+      console.log("Fetch Error :-S", err);
+    });
+});
